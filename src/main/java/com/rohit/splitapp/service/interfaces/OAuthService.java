638b3f3979f71e9b2164212a8ff6828eaebc2f62
Jigar Prajapati <149820949+jdp8951@users.noreply.github.com>
2025-06-11 18:35:07 +0530

    Removed Oauth

--- src/main/java/com/rohit/splitapp/service/interfaces/OAuthService.java
+++
@@ -1,7 +0,0 @@
-package com.rohit.splitapp.service.interfaces;
-
-import com.rohit.splitapp.persistence.dto.user.AuthenticationResponse;
-
-public interface OAuthService {
-    public AuthenticationResponse handleCallback(String code, String state);
-}
